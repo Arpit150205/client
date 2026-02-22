c5edb092917e2a9801babe406d3e50e3c9bc294c
Arpit150205 <arpitbhande@gmail.com>
2026-02-22 17:01:10 +0530

    Initial LMS Upload

---
+++ src/components/student/Companies.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Companies = () => {
+  return (
+    <div>
+      <h1>com aopanies</h1>
+    </div>
+  )
+}
+
+export default Companies
---
+++ src/context/AppContext.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { createContext, useEffect, useState } from "react";
+
+export const AppContext = createContext()
+export const AppContextProvider = (props) => {
+
+
+const value = () => {}
+  
+return (
+        <AppContext.Provider value={value}>
+            {props.children}
+        </AppContext.Provider>
+    )
+
+}
+
+
+export default AppContext
